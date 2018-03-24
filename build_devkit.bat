6af13ef41db3a9a3832e5c3b7679af323b5591c8
capriciousalex <capriciousalex@users.noreply.github.com>
2018-03-24 22:48:37 +0000

    fix build_devkit.bat

--- build_devkit.bat
+++ build_devkit.bat
@@ -3,11 +3,7 @@ COLOR 0A
 
 del /S /q "acb_devkit.vr"
 mkdir "acb_devkit.vr"
-xcopy /s "src" "acb_devkit.vr"
-cd "bin"
-start FileBank.exe -dst "../src" "../acb_devkit.vr"
-timeout 1
-del /f /s /q "../acb_devkit.vr"
-rmdir /s /q "../acb_devkit.vr"
+xcopy /s "src/arma/sqm" "acb_devkit.vr"
+xcopy /s "src/arma/framework" "acb_devkit.vr"
 
 pause
